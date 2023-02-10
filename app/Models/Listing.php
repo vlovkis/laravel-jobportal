a5d9375f967b58b8b6ebeca3ef506d30e339a64b
vlovkis <vlovkis@inbox.lt>
2023-02-10 22:19:20 +0200

    -pridėtas css su html layoutu

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,29 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing{
-    public static function all(){
-        return [
-            [
-            'id' => 1,
-            'title' => 'Skelbimas nr 1',
-            'description' => 'Darbo skelbimo aprasymas'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Skelbimas nr 2',
-                'description' => 'Darbo skelbimo aprasymas'
-            ]
-            ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
+
+        Listing::factory(6)->create();
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Avitela',
+        //     'email' => 'email1@email.com',
+        //     'location' => 'Klaipeda, Lithuania',
+        //     'website' => 'https://www.avitela.lt',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper,
+        //     sapien quis faucibus aliquam, sapien ante elementum lectus, a condimentum ipsum mauris nec turpis.
+        //      Proin lacinia leo eget neque hendrerit, ut posuere mi rutrum. Proin aliquam, justo sed dapibus vulputate, 
+        //      nunc metus venenatis enim, non pretium ligula risus in odio. '
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Php Junior Developer',
+        //     'tags' => 'php, junior',
+        //     'company' => 'CodeAcademy',
+        //     'email' => 'email2@email.com',
+        //     'location' => 'Vilnius, Lithuania',
+        //     'website' => 'https://www.codeacademy.lt',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ullamcorper,
+        //     sapien quis faucibus aliquam, sapien ante elementum lectus, a condimentum ipsum mauris nec turpis.
+        //      Proin lacinia leo eget neque hendrerit, ut posuere mi rutrum. Proin aliquam, justo sed dapibus vulputate, 
+        //      nunc metus venenatis enim, non pretium ligula risus in odio. '
+        // ]);
     }
 }
