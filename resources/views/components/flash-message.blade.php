e4561889e31b68d1444a9eb46849caf23fa72d1a
vlovkis <vlovkis@inbox.lt>
2023-02-16 15:50:16 +0200

    -sutvarkytas flash message

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,4 +1,4 @@
-@if(session()->has('messsage'))
+@if(session('message'))
     <div class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3">
         <p>
             {{session('message')}}
